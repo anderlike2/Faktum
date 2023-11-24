9a5b810b32861b1e3d88e96b4dc9bc78f2f442cc
Anderson <anderlike2@hotmail.com>
2023-11-24 09:13:33 -0500

    ajustes

--- ApiFaktum/RepositoryLayer/Data/DbInitializer.cs
+++ ApiFaktum/RepositoryLayer/Data/DbInitializer.cs
@@ -8,25 +8,6 @@ namespace RepositoryLayer.Data
         {
             ArgumentNullException.ThrowIfNull(dbContext, nameof(dbContext));
 
-            dbContext.Database.EnsureCreated();          
-
-            //************************************
-            //TipoDocumento
-            //************************************
-            if (!dbContext.TipoDocumentos.Any())
-            {
-                var tipoDocumentos = new TipoDocumento[]
-                {
-                    new TipoDocumento { Nombre = "Cédula", FechaCreacion = DateTime.UtcNow, Estado = 1 },
-                    new TipoDocumento { Nombre = "Cédula extranjería", FechaCreacion = DateTime.UtcNow, Estado = 1 },
-                    new TipoDocumento { Nombre = "Tarjeta Identidad", FechaCreacion = DateTime.UtcNow, Estado = 1 }
-                };
-
-                foreach (TipoDocumento s in tipoDocumentos)
-                    dbContext.TipoDocumentos.Add(s);
-
-                dbContext.SaveChanges();
-            }       
-        }
+            dbContext.Database.EnsureCreated();
     }
 }
