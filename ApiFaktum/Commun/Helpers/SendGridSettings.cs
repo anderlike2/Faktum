8f388b1b1ea01f428088fa6d860e878100523518
Anderson <anderlike2@hotmail.com>
2023-11-24 14:22:34 -0500

    Ajustes

--- ApiFaktum/Commun/Helpers/SendGridSettings.cs
+++ ApiFaktum/Commun/Helpers/SendGridSettings.cs
@@ -2,7 +2,7 @@
 {
     public class SendGridSettings
     {
-        public const string SendGrid_Key = "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";
-        public const string SendGrid_fromMailSendGrid = "mtovar@katary.co";
+        public const string SendGrid_Key = "<llaveSenGrind>";
+        public const string SendGrid_fromMailSendGrid = "<correoSengrid>";
     }
 }
--- ApiFaktum/RepositoryLayer/Data/ApplicationDbContext.cs
+++ ApiFaktum/RepositoryLayer/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace RepositoryLayer.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -19,44 +19,7 @@ namespace RepositoryLayer.Data
             }
 
             base.OnModelCreating(modelBuilder);
-
-            this.SeedUsers(modelBuilder);
-            this.SeedRoles(modelBuilder);
-            this.SeedUserRoles(modelBuilder);
         }
         public DbSet<TipoDocumento> TipoDocumentos { get; set; }
-
-        private void SeedUsers(ModelBuilder builder)
-        {
-            ApplicationUser user = new ApplicationUser()
-            {
-                Id = "b74ddd14-6340-4840-95c2-db12554843e5",
-                UserName = "Admin",
-                Email = "reygue28@gmail.com",
-                LockoutEnabled = false,
-                PhoneNumber = "3015267740",
-                EmailConfirmed = true,
-            };
-
-            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
-            user.PasswordHash = passwordHasher.HashPassword(user, "Admin*123");
-
-            builder.Entity<ApplicationUser>().HasData(user);
-        }
-
-        private void SeedRoles(ModelBuilder builder)
-        {
-            builder.Entity<IdentityRole>().HasData(
-                new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
-                new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", Name = "Agente", ConcurrencyStamp = "2", NormalizedName = "Agente" }
-            );
-        }
-
-        private void SeedUserRoles(ModelBuilder builder)
-        {
-            builder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>() { RoleId = "fab4fac1-c546-41de-aebc-a14da6895711", UserId = "b74ddd14-6340-4840-95c2-db12554843e5" }
-                );
-        }
     }
 }
